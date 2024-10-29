8b00eb9daedff262a5b71418778ac4702935a27d
Nadiia_Kucheriava <nadya.net206@gmail.com>
2024-10-30 00:57:55 +0200

    java practise oop

--- classAndObject/src/test/TestRunner.java
+++ classAndObject/src/test/TestRunner.java
@@ -5,7 +5,7 @@ public class TestRunner {
         Car car1 = new Car ("Toyota", "RAV4", 1000.0, 2021);
         Car car2 = new Car ("BMW", "i8", 500.0, 2024);
         System.out.println(car1);
-        System.out.println(car1);
+        System.out.println(car2);
     }
 
 }
